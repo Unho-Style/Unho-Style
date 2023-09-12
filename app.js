0215c0135649189d680d47640ee8d9cb625709be
bass9030 <bass90301.naver.comm@gmail.com>
2023-09-12 23:54:09 +0900

    수정: 로그인을 위한 express-session 미들웨어 추가

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +18,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'fucking! strong_password123',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+	    httpOnly: true,
+        maxAge: 86400000
+    }
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
